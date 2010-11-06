9b18187d71d108870cab995480a0037cc7ac8a29
David Coallier <david@echolibre.com>
2010-11-06 06:18:23 +0000

    - Added the isNeighbourOf example.

---
+++ examples/isNeighbourOf.js
@@ -0,0 +1,19 @@
+var sys    = require('sys');
+var edge   = require('../lib/edge');
+var graph  = require('../lib/graph');
+var vertex = require('../lib/vertex');
+
+var Graph       = new graph.Graph();
+var VertexOne   = new vertex.Vertex('nameOne',   {});
+var VertexTwo   = new vertex.Vertex('nameTwo',   {});
+var VertexThree = new vertex.Vertex('nameThree', {});
+
+Graph.addVertex(VertexOne);
+Graph.addVertex(VertexTwo);
+Graph.addVertex(VertexThree);
+
+Graph.addEdge(new edge.Edge('nameOne', 'nameTwo',   {}));
+Graph.addEdge(new edge.Edge('nameOne', 'nameThree', {}));
+
+var verts = Graph.getVertices();
+sys.puts(sys.inspect(VertexTwo.isNeighbourOf('nameOnex')));
